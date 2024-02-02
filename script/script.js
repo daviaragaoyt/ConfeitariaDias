831220f1a6ce150f464adb5e2ac9d2f5dac6f30f
DAVI <daviaragaoyt@gmail.com>
2024-02-02 16:07:53 -0300

    Carrinho finalizado

--- script/script.js
+++ script/script.js
@@ -1,105 +1,113 @@
 const catalogo = document.getElementById("containerCatalogo"); //Variavel que busca os elementos do "containerCatalogo"
 
-const produtos = [ //Variavel Array com suas propriedades
+const produtos = [
+    //Variavel Array com suas propriedades
     {
         idProduto: 0,
         nome: "Brigadeiro tradicional",
-        preco: "R$15,99",
+        preco: 2.99,
         imgUrl: "imgs/brigadeiro.webp",
-        quantidade: 0,
+        quantidade: 1,
     },
     {
         idProduto: 1,
         nome: "Brigadeiro/Maracujá",
-        preco: "R$15,99",
+        preco: 2.99,
         imgUrl: "imgs/maracuja.jpg",
-        quantidade: 0,
+        quantidade: 1,
     },
     {
         idProduto: 2,
         nome: "Brigadeiro de ninho",
-        preco: "R$15,99",
+        preco: 2.99,
         imgUrl: "imgs/ninhocomnutella.jpg",
-        quantidade: 0,
+        quantidade: 1,
     },
     {
         idProduto: 3,
         nome: "Brigadeiro de Limão",
-        preco: "R$15,99",
+        preco: 2.99,
         imgUrl: "imgs/limao.jpeg",
-        quantidade: 0,
+        quantidade: 1,
     },
     {
         idProduto: 4,
         nome: "Tortas",
-        preco: "R$15,99",
+        preco: 2.99,
         imgUrl: "imgs/tortas.webp",
-        quantidade: 0,
+        quantidade: 1,
     },
     {
         idProduto: 5,
         nome: "Brownie",
-        preco: "R$15,99",
+        preco: 2.99,
         imgUrl: "imgs/brownie.jpg",
-        quantidade: 0,
+        quantidade: 1,
     },
     {
         idProduto: 6,
         nome: "Brigadeiro tradicional",
-        preco: "R$15,99",
+        preco: 2.99,
         imgUrl: "imgs/brigadeiro.webp",
-        quantidade: 0,
+        quantidade: 1,
     },
     {
         idProduto: 7,
         nome: "Brigadeiro tradicional",
-        preco: "R$15,99",
+        preco: 2.99,
         imgUrl: "imgs/brigadeiro.webp",
-        quantidade: 0,
+        quantidade: 1,
     },
     {
         idProduto: 8,
         nome: "Brigadeiro tradicional",
-        preco: "R$15,99",
+        preco: 2.99,
         imgUrl: "imgs/brigadeiro.webp",
-        quantidade: 0,
+        quantidade: 1,
     },
     {
         idProduto: 9,
         nome: "Brigadeiro tradicional",
-        preco: "R$15,99",
+        preco: 2.99,
         imgUrl: "imgs/brigadeiro.webp",
-        quantidade: 0,
+        quantidade: 1,
     },
 ];
 
-produtos.map((item) => { //Inserindo Html no codigo pelo Js, falando que sempre que tiver um elemento no catalago a div sera criada, e passando o item
+produtos.map((item) => {
+    //Inserindo Html no codigo pelo Js, falando que sempre que tiver um elemento no catalago a div sera criada, e passando o item
     catalogo.innerHTML += `<div class="box">
     <img class="img" src=${item.imgUrl} alt />
     <h3>${item.nome}</h3>
-    <div class="price">${item.preco}</div>
-    <button id="candy2" type="submit" class="btn">
-        <a href="cart.html" onclick='adicionarCarrinho(${item.idProduto}, "catalago1")'>Adicionar ao carrinho</a>
+    <div class="price">R$ ${item.preco}</div>
+    <button onclick='adicionarCarrinho(${item.idProduto}, "catalago1")' id="candy2" type="submit" class="btn">
+        <a href="cart.html">Adicionar ao carrinho</a>
     </button>
 </div>`;
 });
 
-function adicionarCarrinho(id, catalogo) { //Função para adicionar os itens e seus elementos para a proxima page(carrinho)
+function adicionarCarrinho(id, catalogo) {
+    //Função para adicionar os itens e seus elementos para a proxima page(carrinho)
     let carrinhoNovo = []; //Array vazio
 
     const carrinhoAtual = JSON.parse(
-        localStorage.getItem("carrinhoMelosConfeitaria") //Quarda as informações do objeto no Coockie 
+        localStorage.getItem("carrinhoMelosConfeitaria") //Quarda as informações do objeto no Coockie
     );
 
-    if (carrinhoAtual != null) { //Se o carrinho atual tiver com itens, permanecerá com os itens
+    if (carrinhoAtual != null) {
+        //Se o carrinho atual tiver com itens, permanecerá com os itens
         carrinhoNovo = carrinhoAtual;
 
-        if (catalogo == "catalago1") {//Verifica se é item do catalago 1
+        if (catalogo == "catalago1") {
+            //Verifica se é item do catalago 1
             carrinhoNovo.push(produtos[id]);
-        } else {//Se não 2
+        }
+        else {
+            //Se não 2
             carrinhoNovo.push(outrosProdutos[id]);
         }
-    } else {//Se tiver vazio, receberá os novos itens
+    } else {
+        //Se tiver vazio, receberá os novos itens
         if (catalogo == "catalago1") {
             carrinhoNovo.push(produtos[id]);
         } else {
@@ -107,35 +115,36 @@ function adicionarCarrinho(id, catalogo) { //Função para adicionar os itens e 
         }
     }
 
-    localStorage.setItem( //Quarda os itens do novo carrinho no Cookie
+    localStorage.setItem(
+        //Quarda os itens do novo carrinho no Cookie
         "carrinhoMelosConfeitaria",
         JSON.stringify(carrinhoNovo)
     );
 }
 
-const catalogo2 = document.getElementById("containerCatalogo2");//A mesma coisa para o catalogo2
+const catalogo2 = document.getElementById("containerCatalogo2"); //A mesma coisa para o catalogo2
 
 const outrosProdutos = [
     {
         idProduto: 0,
         nome: "Brigadeiro de Café",
-        preco: "R$24,99",
+        preco: "24,99",
         imgUrl: "imgs/cafe.jpg",
-        quantidade: 0,
+        quantidade: 1,
     },
     {
         idProduto: 1,
         nome: "Brownie com sorvete",
-        preco: "R$32,50",
+        preco: "32,51",
         imgUrl: "imgs/browniesorvete.jpg",
-        quantidade: 0,
+        quantidade: 1,
     },
     {
         idProduto: 2,
         nome: "Brigadeiro de Paçoca",
-        preco: "R$24,99",
+        preco: "24,99",
         imgUrl: "imgs/paçoca.jpg",
-        quantidade: 0,
+        quantidade: 1,
     },
 ];
 
@@ -160,4 +169,3 @@ outrosProdutos.map((item) => {
     </div >
 </div > `;
 });
-
--- script/scriptCart.js
+++ script/scriptCart.js
@@ -1,31 +1,96 @@
-const carrinho = JSON.parse(localStorage.getItem("carrinhoMelosConfeitaria"));
+let carrinho = JSON.parse(localStorage.getItem("carrinhoMelosConfeitaria"));
+let total = 0;
 
 console.log("Carrinho:", carrinho);
 
 const tabelaCarrinho = document.getElementById("tabelaCarrinho");
 
-carrinho.map((item) => {
-  tabelaCarrinho.innerHTML += `<tr>
+function criaTabela() {
+  tabelaCarrinho.innerHTML = "";
+  carrinho.map((item) => {
+    tabelaCarrinho.innerHTML += `
+  <tr class="cart-produto">
     <td>
       <div class="product">
         <img style="width:150px; heigth:150px;   " src=${item.imgUrl} />
         <div class="info">
           <div class="name">${item.nome}</div>
-          <div class="category">Categoria</div>
+          <div class="category">A Unidade</div>
         </div>
       </div>
     </td>
-    <td>${item.preco}</td>
+    <td class="cart-product-preco">R$${item.preco}</td>
     <td>
       <div class="qty">
-        <button class="menos"><i class="bx bx-minus"></i></button>
-        <span>${item.quantidade}</span>
-        <button class="mais"><i class="bx bx-plus"></i></button>
+        <button onclick='alteraQuantidade(${item.idProduto}, "subtrair")' class="menos">
+        <i class="bx bx-minus"></i>
+        </button>
+        <span id="contadorItem${item.idProduto}">${item.quantidade}</span>
+        <button onclick='alteraQuantidade(${item.idProduto}, "somar")' class="mais">
+        <i class="bx bx-plus"></i>
+        </button>
       </div>
     </td>
     <td></td>
     <td>
-      <button class="remove"><i class="bx bx-x"></i></button>
+      <button onclick="removerProduto(${item.idProduto})" class="remove"><i class="bx bx-x"></i></button>
     </td>
   </tr>`;
-});
+  });
+
+  calculaTotal();
+}
+
+criaTabela();
+
+function alteraQuantidade(idProduto, operacao) {
+  const contadorItem = document.getElementById(`contadorItem${idProduto}`);
+
+  carrinho.map((item) => {
+    if (item.idProduto == idProduto) {
+      if (operacao == "subtrair" && item.quantidade > 1) {
+        item.quantidade--;
+        contadorItem.innerHTML = item.quantidade;
+      }
+
+      if (operacao == "somar") {
+        item.quantidade++;
+        contadorItem.innerHTML = item.quantidade;
+      }
+    }
+  });
+
+  localStorage.setItem(
+    //Quarda os itens do novo carrinho no Cookie
+    "carrinhoMelosConfeitaria",
+    JSON.stringify(carrinho)
+  );
+
+  calculaTotal();
+}
+
+calculaTotal();
+
+function calculaTotal() {
+  total = 0;
+  const elementoTotal = document.getElementById("total");
+
+  carrinho.map((item) => {
+    total = total + item.quantidade * item.preco;
+  });
+
+  elementoTotal.innerHTML = total.toFixed(2);
+}
+
+function removerProduto(id) {
+  let novoCarrinho = carrinho.filter((item) => item.idProduto !== id);
+  carrinho = novoCarrinho;
+
+  localStorage.setItem(
+    //Quarda os itens do novo carrinho no Cookie
+    "carrinhoMelosConfeitaria",
+    JSON.stringify(carrinho)
+  );
+
+  criaTabela();
+}
